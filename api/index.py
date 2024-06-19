25147754786234792c0fa092388c696b5b87371a
Andreyhuey <andreyhuey777@gmail.com>
2024-06-19 15:10:28 +0100

    added parameters

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+# app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/python")
 def hello_world():
