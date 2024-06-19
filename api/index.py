b3e787082794c78e955e16bed30d8c8207c434d7
Andreyhuey <andreyhuey777@gmail.com>
2024-06-19 14:56:20 +0100

    add: testing status code

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/hi", status_code=status.HTTP_200_OK)
+def hi():
+    return {"message": "testing"}
